2ab326a4ccd40f9d0c3c3933dc7f0d1406f78e18
gabiheckmann <gabimollo123@gmail.com>
2025-08-20 08:30:16 -0300

    Criando Banco

---
+++ script.sql
@@ -0,0 +1,34 @@
+-- criando banco de dados -- 
+create database EcomLoja;
+use EcomLoja;
+
+create table produto(
+ Id int primary key auto_increment,
+ Nome varchar(40),
+ Descricao varchar(40),
+ Preco decimal(10,2),
+ ImageUrl varchar(255),
+ Estoque int not null
+ );
+
+create table pedido(
+Id int primary key auto_increment ,
+DataPedido datetime,
+Total decimal(10,2),
+Status varchar(50),
+Endereco varchar(100),
+FormaPagamento varchar(100),
+Frete decimal (10,2)
+);
+
+create table itemPedido(
+Id int primary key auto_increment ,
+PedidoId int,
+ProdutoId int,
+Quantidade int,
+PrecoUnitario decimal(10,2)
+);
+
+select * from produto;
+select * from pedido;
+select * from itemPedido;
